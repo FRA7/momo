678f930da7767848ff7ba355a4657a6759a24bdd
蒋云皓 <458734857@qq.com>
2016-01-26 22:54:20 +0800

    创建项目

---
+++ momo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo2
+//
+//  Created by Francis on 16/1/26.
+//  Copyright © 2016年 FRAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
