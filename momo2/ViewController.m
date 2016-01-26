719a136baf28404041b769c02670eeaa2770c323
蒋云皓 <458734857@qq.com>
2016-01-26 22:58:33 +0800

    皓哥帅帅帅

--- momo2/ViewController.m
+++ momo2/ViewController.m
@@ -15,13 +15,10 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //皓哥帅帅帅
+    NSLog(@"皓哥帅帅帅");
+    
 }
 
 @end
